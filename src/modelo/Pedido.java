36e804c0124811854c0f5904fffc3e6ca8ee8651
zepedaG06 <jazepeda@uamv.edu.ni>
2026-04-29 20:23:08 -0600

    Clase pedido realizado

--- src/modelo/Pedido.java
+++ src/modelo/Pedido.java
@@ -1,4 +1,37 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Pedido {
+    private ArrayList<Producto> productos;
+    private String estado;
+
+    public Pedido() {
+        productos = new ArrayList<>();
+        estado = "Pendiente";
+    }
+
+    public void agregarProducto(Producto p) {
+        productos.add(p);
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for (Producto p : productos) {
+            total += p.getPrecio();
+        }
+        return total;
+    }
+
+    public void cambiarEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public ArrayList<Producto> getProductos() {
+        return productos;
+    }
 }
