bee5057f01f8272a86f9f4c610eb9f943745e7e5
zepedaG06 <jazepeda@uamv.edu.ni>
2026-04-29 20:27:18 -0600

    Clase cocina listo

--- src/servicios/Cocina.java
+++ src/servicios/Cocina.java
@@ -1,4 +1,17 @@
 package servicios;
 
+import modelo.Pedido;
+import javax.swing.JOptionPane;
+
 public class Cocina {
+
+    public void procesarPedido(Pedido pedido) {
+        JOptionPane.showMessageDialog(null, "Cocina: Procesando pedido...");
+        pedido.cambiarEstado("En proceso");
+
+        JOptionPane.showMessageDialog(null, "Cocina: Preparando productos...");
+
+        pedido.cambiarEstado("Listo");
+        JOptionPane.showMessageDialog(null, "Cocina: Pedido listo");
+    }
 }
