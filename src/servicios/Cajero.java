0e64a2be002efb6672bebb21bd2c498562c0d38b
zepedaG06 <jazepeda@uamv.edu.ni>
2026-04-29 20:28:12 -0600

    Clase cajero listo

--- src/servicios/Cajero.java
+++ src/servicios/Cajero.java
@@ -1,4 +1,23 @@
 package servicios;
 
+import modelo.Cliente;
+import modelo.Pedido;
+import javax.swing.JOptionPane;
+
 public class Cajero {
+
+    public void registrarPedido(Pedido pedido) {
+        JOptionPane.showMessageDialog(null, "Cajero: Registrando pedido...");
+    }
+
+    public void enviarPedidoACocina(Pedido pedido, Cocina cocina) {
+        JOptionPane.showMessageDialog(null, "Cajero: Enviando pedido a cocina...");
+        cocina.procesarPedido(pedido);
+    }
+
+    public void notificarCliente(Cliente cliente, Pedido pedido) {
+        JOptionPane.showMessageDialog(null,
+                "Cajero: " + cliente.getNombre() +
+                        ", tu pedido está " + pedido.getEstado());
+    }
 }
