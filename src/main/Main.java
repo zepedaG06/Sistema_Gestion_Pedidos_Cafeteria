9e32926562fe2b332a2a541e10a59afc55eaaf68
zepedaG06 <jazepeda@uamv.edu.ni>
2026-04-29 20:29:38 -0600

    Clase main listo

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,28 @@
 package main;
 
+import modelo.Cliente;
+import modelo.Pedido;
+import servicios.Cajero;
+import servicios.Cocina;
+
+import javax.swing.JOptionPane;
+
 public class Main {
+    public static void main(String[] args) {
+
+        String nombre = JOptionPane.showInputDialog("Ingrese nombre del cliente:");
+        Cliente cliente = new Cliente(nombre);
+
+        Cajero cajero = new Cajero();
+        Cocina cocina = new Cocina();
+
+        // Flujo del sistema
+        Pedido pedido = cliente.realizarPedido();
+        cajero.registrarPedido(pedido);
+        cajero.enviarPedidoACocina(pedido, cocina);
+        cajero.notificarCliente(cliente, pedido);
+
+        JOptionPane.showMessageDialog(null,
+                "Total a pagar: $" + pedido.calcularTotal());
+    }
 }
